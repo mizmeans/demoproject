339c6e6c5913b6cd35c133db154b31135b6b0a76
Megan Means <megan.means@cerner.com>
2017-08-30 17:12:10 -0500

    Validated with basic bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,15 +3,21 @@
 //var path = require('path');
 //var open = require('open');
 
-//EC6
+//EC6 - build scripts will be transpiled
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-
-//EC5 uses var, EC6 uses const
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
